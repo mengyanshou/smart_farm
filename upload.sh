3a8c7c70506910a11f215ea2c7287a3938def177
Nightmare-MY <906262255@qq.com>
2021-05-04 23:21:07 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 
-scp -r build/web/* root@nightmare.fun:/usr/local/tomcat/webapps/fluttersmartfarm
+scp -r build/web/* root@nightmare.fun:/home/apache-tomcat-10.0.2/webapps/fluttersmartfarm
 #  ffmpeg -f avfoundation -pixel_format uyvy422 -i "1" -f flv rtmp://localhost:1932/live/room1
